e999ccb6a4ec044b5b62ca02dc6cd43647a6ef95
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-10-30 01:51:23 +0900

    View, Controller 세팅

---
+++ src/main/java/com/example/shop/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+}
