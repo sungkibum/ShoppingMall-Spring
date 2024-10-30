96e78b3c7122470b6b3f376d9abf1a5ade8c36a8
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-10-30 18:19:14 +0900

    Lombok 학습

--- src/main/java/com/example/shop/controller/HelloController.java
+++ src/main/java/com/example/shop/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.shop.controller;
 
+import com.example.shop.dto.UserDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,11 @@ import java.awt.*;
 public class HelloController {
 
     @GetMapping("/")
-    public String hello() {
-        return "hello";
+    public UserDto hello() {
+        UserDto userDto = new UserDto(10, "김길동");
+        userDto.setAge(20);
+        userDto.setName("홍길동");
+        System.out.println("userDto : " + userDto);
+        return userDto;
     }
 }
---
+++ src/main/java/com/example/shop/dto/UserDto.java
@@ -0,0 +1,16 @@
+package com.example.shop.dto;
+
+import lombok.*;
+
+
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private int age;
+    private String name;
+
+}
