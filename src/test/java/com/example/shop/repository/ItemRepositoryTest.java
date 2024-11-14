23b5ec18f7df866bb77a0ff8638ffd7028ca732c
성기범 <133200703+sungkibum@users.noreply.github.com>
2024-11-15 00:42:15 +0900

    상품 이름 검색 테스트 코드 작성

--- src/test/java/com/example/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/example/shop/repository/ItemRepositoryTest.java
@@ -9,16 +9,31 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@Transactional
 class ItemRepositoryTest {
 
     @Autowired
     private ItemRepository itemRepository;
 
+    public void createItemList() {
+        for (int i = 1; i <= 10; i++) {
+            Item item = Item.builder()
+                    .itemNm("테스트 상품" + i)
+                    .price(10000 + i)
+                    .stockNumber(100 + i)
+                    .itemDetail("테스트 상품 상세 설명" + i)
+                    .itemSellStatus(ItemSellStatus.SELL)
+                    .regTime(LocalDateTime.now())
+                    .updateTime(LocalDateTime.now())
+                    .build();
+
+            itemRepository.save(item);
+        }
+    }
     @Test
     @DisplayName("상품 생성 테스트")
     public void createItemTest() {
@@ -34,5 +49,23 @@ class ItemRepositoryTest {
         System.out.println("======== item: " + item);
         Item savedItem = itemRepository.save(item);
         System.out.println("======== saveditem: " + savedItem);
+    }
+
+    @Test
+    @DisplayName("상품 이름 검색 테스트")
+    public void findByItemNmTest() {
+        createItemList();
+
+        itemRepository
+                .findByItemNm("테스트 상품1")
+                .forEach((item -> {
+                    System.out.println(item);   //람다식 표현
+                }));
+        //아래 식 변형
+//        for (Item item : itemList) {
+//            System.out.println(item);
+//        }
+
+
     }
 }
